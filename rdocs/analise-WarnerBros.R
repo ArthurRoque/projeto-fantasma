aa55d4dd72e09754de803b489524cd54fb5bda28
ArthurRoque <arthurcarvalhoroque@gmail.com>
2024-04-26 09:48:39 -0300

    Transformando vas's em quantitativas

--- rdocs/analise-WarnerBros.R
+++ rdocs/analise-WarnerBros.R
@@ -1,3 +1,8 @@
 warner_bros <- read.csv2('banco/banco_final.csv', sep=',')
-summary(warner_bros)
 
+# Transformando variáveis quantitativas
+vas_quantitativas <- c('imdb', 'engagement')
+warner_bros[, vas_quantitativas] <- lapply(warner_bros[, vas_quantitativas], as.numeric)
+
+summary(warner_bros$imdb)
+summary(warner_bros$engagement)
